1f287963394dd3877b41edc574c2170ac6863e75
Swapnil Katwe <swapnil.katwe111@gmail.com>
2025-01-29 13:50:44 +0100

    Test cases for UI: Should render 5x5 grid Should renders keyboard with letters, backspace and enter Should update grid with letters Should not allow to enter more than 5 letters.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,57 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+
 import App from "./App";
 
 describe("App", () => {
+
   it("renders the App component", () => {
     render(<App />);
+    expect(screen.getByText(/Wordle game/i)).toBeInTheDocument();
+  });
 
-    screen.debug(); // prints out the jsx in the App component unto the command line
+  it("renders the 5x5 grid", () => {
+    render(<App />);
+    const cells = screen.getAllByTestId("textbox");
+    expect(cells).toHaveLength(25);
+  });
+
+  it("renders a keyboard with letters A-Z backspace and enter", () => {
+    render(<App />);
+    const keys = screen.getAllByRole("button");
+    expect(keys).toHaveLength(28); // A-Z keys, backspace, and enter
+  });
+
+  it("updates the grid when typing letters", async () => {
+    render(<App />);
+    const keys = screen.getByText("A");
+    const firstCell = screen.getAllByRole("textbox")[0];
+    await userEvent.click(keys);
+    expect(firstCell).toHaveValue("A");
+  });
+
+  it("dose not allow to enter more than 5 characters", () => {
+    render(<App />);
+    const keys = ["A", "B", "C", "D", "E"].map((letter) =>
+      screen.getByText(letter)
+    );
+    keys.forEach((key) => userEvent.click(key));
+    const extraKey = screen.getByText("F");
+    userEvent.click(extraKey);
+    const row = screen.getAllByRole("textbox").slice(0, 5);
+    expect(row.map((cell) => cell.value)).not.toContain("F");
+  });
+  
+  it("clears each cell when clicking on backspace", async () => {
+    render(<App />);
+    const keys = ["A", "B", "C", "D", "E"].map((letter) =>
+      screen.getByText(letter)
+    );
+    keys.forEach((key) => userEvent.click(key));
+    const backspace = screen.getByText("Backspace");
+    await userEvent.click(backspace);
+
+    const row = screen.getAllByRole("textbox").slice(0, 5);
+    expect(row.map((cell) => cell.value)).toEqual(["A", "B", "C", "D", ""]);
   });
 });
