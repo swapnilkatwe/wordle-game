1d9c4f9c8dde30e8ae0c398bd7cb4f75c2a9b629
Swapnil Katwe <swapnil.katwe111@gmail.com>
2025-01-29 23:41:26 +0100

    Test cases for util function compareGuess. All test scenarios are covered. compareGuress function for compare guess and selected word and returns status array.

---
+++ src/utils/utils.test.ts
@@ -0,0 +1,47 @@
+import { describe, it, expect } from "vitest";
+import { compareGuess } from "./utils";
+
+describe("compareGuess", () => {
+  it("should return correct letter status for each letter in the guess", () => {
+    const word = "ehlla";
+    const guess = "hello";
+    const expected = ["present", "present", "correct", "correct", "absent"];
+    expect(compareGuess(word, guess)).toEqual(expected);
+  });
+});
+
+describe("compareGuess", () => {
+  it("should return all correct letters for correct guess", () => {
+    const word = "hello";
+    const guess = "hello";
+    const expected = ["correct", "correct", "correct", "correct", "correct"];
+    expect(compareGuess(word, guess)).toEqual(expected);
+  });
+});
+
+describe("compareGuess", () => {
+  it("should return all absent letters for all wrong guess", () => {
+    const word = "fruit";
+    const guess = "hello";
+    const expected = ["absent", "absent", "absent", "absent", "absent"];
+    expect(compareGuess(word, guess)).toEqual(expected);
+  });
+});
+
+describe("compareGuess", () => {
+  it("should handle only one present if you have more of a letter than in the correct answer", () => {
+    const word = "water";
+    const guess = "otter";
+    const expected = ["absent", "absent", "correct", "correct", "correct"];
+    expect(compareGuess(word, guess)).toEqual(expected);
+  });
+});
+
+describe("compareGuess", () => {
+  it("should return correct letter", () => {
+    const word = "colon";
+    const guess = "allol";
+    const expected = ["absent", "absent", "correct", "correct", "absent"];
+    expect(compareGuess(word, guess)).toEqual(expected);
+  });
+});
---
+++ src/utils/utils.ts
@@ -0,0 +1,27 @@
+export enum letterStatus {
+  correct = "correct",
+  present = "present",
+  absent = "absent",
+}
+
+export function compareGuess(word: string, guess: string): letterStatus[] {
+  const wordArray = word.split("");
+  const guessArray = guess.split("");
+
+  return guessArray.map((letter, index) => {
+    const guessLetterCount = guessArray.filter((l) => l === letter).length;
+    const wordLetterCount = wordArray.filter((l) => l === letter).length;
+
+    if (guessLetterCount <= wordLetterCount) {
+      return letter === wordArray[index]
+        ? letterStatus.correct
+        : letterStatus.present;
+    } else if (guessLetterCount > wordLetterCount) {
+      return letter === wordArray[index]
+        ? letterStatus.correct
+        : letterStatus.absent;
+    } else {
+      return letterStatus.absent;
+    }
+  });
+}
